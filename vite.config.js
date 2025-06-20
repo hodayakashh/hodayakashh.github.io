1c6ca8ed292574bc62b4fec1304d0a59f91e9113
Hodaya <hodaya.kashkash@live.biu.ac.il>
2025-06-20 03:47:26 +0300

    ג

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/HodayaKashkash/',
   plugins: [react()],
   server: {
     allowedHosts: true
@@ -21,4 +20,4 @@ export default defineConfig({
       },
     },
   },
-}) 
+})
