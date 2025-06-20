7a1e974cb1443484d91bc758d67773e5cd801f04
Hodaya <hodaya.kashkash@live.biu.ac.il>
2025-06-20 03:30:49 +0300

    י

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,7 @@ import Studies from "./Studies";
 
 import Year from "./Year";
 
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 
 const PAGES = {
     
